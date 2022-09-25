1fa6bc0999b03cb056c330f76f026bc7286497bc
cratas <petr.kratochvil.st1@vsb.cz>
2022-09-25 19:02:46 +0200

    tree completed

--- lab01.py
+++ lab01.py
@@ -3,10 +3,13 @@ import numpy as np
 INPUT_FILENAME = "test.dat"
  
 class Node:
-  def __init__(self, value, parent, childs):
-    self.value = value
-    self.parent = parent
-    self.childs = childs
+    def __init__(self, value, parent, childs):
+        self.value = value
+        self.parent = parent
+        self.childs = childs
+
+    def add_child(self, child):
+        self.childs.append(child)
  
 class MatrixBuilder:
     def __init__(self, filename):
@@ -41,11 +44,38 @@ class MatrixBuilder:
             loaded_data_list.append(list(map(int, filtered_list))) # parse into integers
  
         return loaded_data_list
- 
+
+class Tree():
+    def __init__(self, headers, root):
+        self.headers = headers
+        self.root = root
+
+    def generate_tree(self, node, depth=0):
+        if depth > 3:
+            return
+
+        for x in range(node.value + 1, self.headers[-1] + 1):
+            new_node = Node(x, node, [])
+            self.generate_tree(new_node, depth+1)
+            node.add_child(new_node)
+
+    def print_tree(self):
+        for x in self.root.childs:
+            print(x.value)
+            for y in x.childs:
+                print("----" + str(y.value))
+                for z in y.childs:
+                    print("--------" + str(z.value))
+
 def main():
     matrix_builder = MatrixBuilder(INPUT_FILENAME)
     matrix = matrix_builder.get_matrix()
-    print(matrix)
- 
+
+    root = Node(0, None, [])
+    tree = Tree(list(range(1, len(matrix[0]) + 1)), root)
+
+    tree.generate_tree(tree.root)
+    tree.print_tree()
+
 if __name__ == "__main__":
     main()
